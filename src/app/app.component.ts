bb9f0116913a29ae6a81515abcb44f2976ae54f0
adsweb4you <77510467+adsweb4you@users.noreply.github.com>
2022-03-13 18:34:57 +0400

    jp17ang

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+
 })
 export class AppComponent {
-  title = 'ang17';
+  title = 10;
+  showcard = true;
+
+  myname(y:any){
+    return y
+  }
+
+  showdiv(){
+   this.showcard = false;
+  }
+
 }
